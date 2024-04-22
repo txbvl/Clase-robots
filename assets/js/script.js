8ceb683f0c7ac75efd9322562c6cfcc20a5459ae
txbvl <toobalgames@gmail.com>
2024-04-22 15:19:44 -0400

    actividad robots 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,20 @@ const {id, name, series } = robots[0];
 console.log(id);
 console.log(name);
 console.log(series);
+
+const getRobotById = (id= "")=> {
+    return robots.find ((robot)=>{
+        return robot.id === id;
+    });
+}
+
+console.log(getRobotById("013"))
+
+const getRobotBySeries = (series= 2) =>{
+    return robots.filter((robot) => {
+        return robot.series === series;
+    })
+}
+
+console.log(getRobotBySeries())
+console.log(getRobotBySeries(3))
