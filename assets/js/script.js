aa4a4be7b449717eeb6745030c394f9c6874af64
unknown <SSDD@PNAWSA06LC1027.duoc.acad>
2024-04-22 15:00:12 -0400

    actividad robots

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+import {robots} from './robots.js';
+
+console.log(robots[0]);
+
+console.log(robots[0].id);
+console.log(robots[0].name);
+console.log(robots[0].series);
+
+const {id, name, series } = robots[0];
+
+console.log(id);
+console.log(name);
+console.log(series);
